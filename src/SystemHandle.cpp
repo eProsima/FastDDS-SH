ded3c7187b866d17f1e57ed5cae2f669d60d3617
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2019-04-30 11:23:33 +0200

    System handle basic structure

---
+++ src/SystemHandle.cpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2018 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include "SystemHandle.hpp"
+
+#include <soss/Mix.hpp>
+#include <soss/Search.hpp>
+
+#include <iostream>
+#include <thread>
+
+namespace soss {
+namespace fastrtps{
+
+bool SystemHandle::configure(
+    const RequiredTypes& /* types */,
+    const YAML::Node& /* configuration */)
+{
+    std::cout << "[soss-fastrtps]: configured!" << std::endl;
+    return true;
+}
+
+bool SystemHandle::okay() const
+{
+    return true;
+}
+
+bool SystemHandle::spin_once()
+{
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(100ms);
+    return okay();
+}
+
+bool SystemHandle::subscribe(
+    const std::string& topic_name,
+    const std::string& message_type,
+    SubscriptionCallback /* callback */,
+    const YAML::Node& /* configuration */)
+{
+    std::cout << "[soss-fiware]: subscriber created. "
+        "topic: " << topic_name << ". "
+        "type: " << message_type << ". "
+        << std::endl;
+
+    return true;
+}
+
+std::shared_ptr<TopicPublisher> SystemHandle::advertise(
+    const std::string& topic_name,
+    const std::string& message_type,
+    const YAML::Node& /* configuration */)
+{
+    std::cout << "[soss-fiware]: publisher created. "
+        "topic: " << topic_name << ". "
+        "type: " << message_type << ". "
+        << std::endl;
+
+    return std::shared_ptr<TopicPublisher>();
+}
+
+
+} // namespace fastrtps
+} // namespace soss
+
+SOSS_REGISTER_SYSTEM("fastrtps", soss::fastrtps::SystemHandle)
---
+++ src/SystemHandle.hpp
@@ -0,0 +1,68 @@
+﻿/*
+ * Copyright (C) 2018 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef SOSS__FASTRTPS__INTERNAL__SYSTEMHANDLE_HPP
+#define SOSS__FASTRTPS__INTERNAL__SYSTEMHANDLE_HPP
+
+//#include "Subscriber.hpp"
+//#include "Publisher.hpp"
+
+#include <soss/SystemHandle.hpp>
+
+#include <vector>
+#include <memory>
+
+namespace soss {
+namespace fastrtps {
+
+
+class SystemHandle : public virtual TopicSystem
+{
+public:
+    SystemHandle() = default;
+    virtual ~SystemHandle() override = default;
+
+    bool configure(
+        const RequiredTypes& types,
+        const YAML::Node& configuration) override;
+
+    bool okay() const override;
+
+    bool spin_once() override;
+
+    bool subscribe(
+        const std::string& topic_name,
+        const std::string& message_type,
+        SubscriptionCallback callback,
+        const YAML::Node& configuration) override;
+
+    std::shared_ptr<TopicPublisher> advertise(
+        const std::string& topic_name,
+        const std::string& message_type,
+        const YAML::Node& configuration) override;
+
+private:
+    //std::vector<std::shared_ptr<Publisher>> publishers_;
+    //std::vector<std::shared_ptr<Subscriber>> subscribers_;
+};
+
+
+} // namespace fastrtps
+} // namespace soss
+
+
+#endif // SOSS__FASTRTPS__INTERNAL__SYSTEMHANDLE_HPP
