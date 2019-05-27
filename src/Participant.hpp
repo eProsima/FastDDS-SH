e5003902bd3e3dc704c33b67e42ba0c4aef11a4c
Luis Muñoz <luismunoz@eprosima.com>
2019-05-27 14:26:06 +0200

    Feature/eprosima license

--- src/Participant.hpp
+++ src/Participant.hpp
@@ -1,5 +1,5 @@
-﻿/*
- * Copyright (C) 2018 Open Source Robotics Foundation
+/*
+ * Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
