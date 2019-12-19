874971b384100adfc3ebf5fac18d3e3fc768feb4
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2019-12-19 07:37:49 +0100

    Update documentation design to xtypes porting.

--- doc/design.md
+++ doc/design.md
@@ -2,20 +2,23 @@
 
 ## Proyect layout:
 - `Dockerfile`: Dockerfile with a system already configured with ROS2
-- `dds`: main system handler
-    - `src` folder: contains the implementation of *soss-dds* library.
-    - `doc` folder: contains the design and user documentation.
-    - `sample` folder: contains a *.yaml* file example and utils for running *soss-dds* easily.
-    - `utils` folder: contains scripts and utilities.
-- `dds-test`: system handler for testing the `dds` system handler.
-    - `src` folder: contains the test sources
-    - `resources` folder: contains the *yaml* configurations for the tests.
-    - `thirdparty` folder: contains the external code used and distributed with the project.
-        - `catch2` folder: contains the *catch2* test framework
+- `doc` folder: contains the design and user documentation.
+- `examples` folder: contains a *.yaml* file example and utils for running *soss-dds* easily.
+- `packages`: colcon packages for this middleware
+    - `dds`: main system handle
+        - `src` folder: contains the implementation of *soss-dds* library.
+        - `test` folder: contains the internal unitary tests for this module.
+        - `thirdparty` folder: contains the external code used and distributed with the project.
+            - `catch2` folder: contains the *fastrtps* as long as any fastrtps can be found.
+    - `dds-test`: system handle for testing the `dds` system handle.
+        - `src` folder: contains the test sources
+        - `resources` folder: contains the *yaml* configurations for the tests.
+        - `thirdparty` folder: contains the external code used and distributed with the project.
+            - `catch2` folder: contains the *catch2* test framework
 
 ## Class diagram
-![](http://www.plantuml.com/plantuml/png/hLFH2fim57ttAqBtebkLlXMAI65GzZZm0oGcTovKav0RosJittTdeurrx6a-tPnpSi-vCRcYvywFGvyazOGUXYzSonuSEvoAGX_OnpolsDUnxHL-1_SFJJssA9ngTw9d1p9HGbck_HjT51MJtdA7j3E9TApCyyZvdOdyabKrT_jiz0CSAgEJLRHZ4j5pHBRDyZDXz7rYmkXlgXiTf1apjCCf2O4oTpxjKRHADqOB-0krHDXJNDBQAznGlvPeqS0XMCwanCl5leW03zoMCoKdpGSb6ZzPm2hOEEWKUd1DpAPKmf6re-ebk6mPt07ti0ebztmMPb6izQmteLOu1kC-0VV64a0mBDa1aNSmXmefxBDN-cCU8_AEVkV671VyVxWx4A0UaCQdrGJTAqjqbrXPihG-VhZTpkVJAMErqbqFdeYgMgyK9LgQC3I8ZJSDOSkCGVLSaNZc_-XIgZVkbeixrUkR8GT2hw0bFUBV)
+![](http://www.plantuml.com/plantuml/png/dLHDYzim4BtxLmpRYzF68RU54Cp2BakFXVHknjAiLwmj6SquD9J-z_f32ALsUsWkaUUzUVDcnl8J2tePkZRBBZSMwhwYrIrvU3WU3lO1FXT52T-6kZNyJluVdCjGi_AcNf4M1VHYbEdHIfOb3t0kYaJ-3oGLpI8B3eSIdfszacVZR1P9AoHJBJB3lP-V6Oo_Bw2SVJFDu2dVWHiplD4K8FU1jtMKUsChPnLjGC721eHwV1R3Tz2lO2sTIp1Mm1kod4vzWYJdmEcmFxDuOvjzuB_SB7P6ZLCQv_7zrbC9udgZU2DZ-G-4Ibmb8p_uRKWghRbAQkQxW7bg30lvIY_5vhnyFD8Uwi6qcdoWWlA4Gf6eKN0cxf8oAvLTCfYuxVknn7WQ3Rs_e608i-ZJQMapT3dfXzhzef7_c0hpAKtVs55y2LTU8l3_PWe8z6bZq14pDXJN0V46z6ASVSQshL5zfB9NKkByUJxBkoVK9NXID2TKIuIhk3z5vahvOZzjEtQRN1yudo6_x3-zlZu-7eyxk8p-Mv8HACjuYrPIm8vvUgUqviq9XyGD2BIUCoU5ltXy8gldZzLl)
 
 ## Sequence diagram
-![](http://www.plantuml.com/plantuml/png/hLN1Yjmm3BtxAmozj8U1deVEicrXwR7GtaenZgtT4MJiO2k1zEihjvE9Od3O3jtBEibxqjEJfNdCP1EDbrvzYfq-FEYFFJhGHts6le_g4OBdZolAEef9Hpf3ClbyVNGnWHeOWGu76qX_cpF1vOiDljz--BgUj-_7hiVCGRTXnYwxX5tr-dCC4wICCMnUU-Adp5W8X3HhxxDIfvF6W8Isnr-McA9F9rNqbbgbp2DN4PxnPKnGgiB94cpha0_CbhKSjJ4bZJ5ltzMucwwZxgFr35Z2huetb0-7W-3uETWBEaFp05bvQ91RkXAy87zEHiQH6cH7nBaPQ0bGIzwYCraQLxbLdk9_uLGDg4Yr_ussMJy1XywlRDjwrMJJMfA-nm7TZV6lql9oVrydJTntgUt_sfK732O6rz9qPpAUCufbvCfyBUkXidluUvItRxNsTcAIc73E77Cs50t75am14TIYMtAPOwYqa8NlCmy_thpdlwUddm-5nMaX-3NjKb7984kIjjGL5K-UKxmihfH_CetiTW87E84f-AfIWLFhKRWaAZLiLiCboQrjOjQkJrNUnOmhyNhB1sAaSPqC8QGNDpnaIgFRNyF6_875kTpzkmP7ydUdNmBtjxk0th4xnr1lpyPYR8tNyZkir6y0)
+![](http://www.plantuml.com/plantuml/png/hLN1YXin3BtxAmPwQG-1dfTcMIWiFHRQMod2iRMxWiGUR4rW-lMrQ2OQ3VMo3Sqbdh6UtlEpf7cex0jFfwFpWNCPChzWWVfoUGove1jR10KQVUBXsrmPJrzyYaUxyLNNzlrqE59jIMsQwL13eSFczUUSpbWgvUGmCV4yn5YTs-y7IiJaZ_JBSziTzdidqeRTlL5qrmId9tgU2ie1wEmPhUJrWw1Niky60A41UF_XzRY2pzIs2yGv-HC5u7d41beeZHchTJ-H9DYoVjhjR7HbprDr4V_A8tu8nXgo1MtzwDZeunaBKtsZZNhBniIEfEzvf715_AkqkfJE6wLfgLbj_zEkEbA2d49FqsraKlUSfGMatfVEtDWz_bckgq-jlQu4fCtLc7Ejm1bQxZLIGnGgkWssOO3b2bLu1xdxyUvZ-pq-_hmNLAF559NsUY9XkAAsjjWJYITF9PyML-HFQUnj5mn8PmI9tvvKuvmkroxfIK7CwhYaM6kJMjptoV8kPbo0boeVkKL2E6WA8rtSY5YvJE7riGWWOINFKlitD8x5Txk_4zYPjXfwo-oSqdPwTWPZjxqUC4NvyhdV)
 
--- packages/dds/src/SystemHandle.cpp
+++ packages/dds/src/SystemHandle.cpp
@@ -15,7 +15,7 @@
  *
 */
 
-#include "SystemHandle.hpp"
+#include <soss/SystemHandle.hpp>
 
 #include "Participant.hpp"
 #include "Publisher.hpp"
@@ -30,97 +30,106 @@
 namespace soss {
 namespace dds{
 
-SystemHandle::~SystemHandle() = default;
-
-bool SystemHandle::configure(
-    const RequiredTypes& /* types */,
-    const YAML::Node& configuration,
-    TypeRegistry& /*type_registry*/)
+class SystemHandle : public virtual TopicSystem
 {
-    try
+public:
+    bool configure(
+        const RequiredTypes& /* types */,
+        const YAML::Node& configuration,
+        TypeRegistry& /*type_registry*/)
     {
-        if (configuration["participant"])
+        try
         {
-            participant_ = std::make_unique<Participant>(configuration["participant"]);
+            if (configuration["participant"])
+            {
+                participant_ = std::make_unique<Participant>(configuration["participant"]);
+            }
+            else
+            {
+                std::cout << "[soss-dds]: participant not provided in configuration file. " <<
+                    "UDP default participant will be created." << std::endl;
+
+                participant_ = std::make_unique<Participant>();
+            }
         }
-        else
+        catch(DDSMiddlewareException& e)
         {
-            std::cout << "[soss-dds]: participant not provided in configuration file. " <<
-                "UDP default participant will be created." << std::endl;
-
-            participant_ = std::make_unique<Participant>();
+            std::cerr << "[soss-dds]: " << e.what() << std::endl;
+            return false;
         }
-    }
-    catch(DDSMiddlewareException& e)
-    {
-        std::cerr << "[soss-dds]: " << e.what() << std::endl;
-        return false;
-    }
 
-    std::cout << "[soss-dds]: configured!" << std::endl;
-    return true;
-}
-
-bool SystemHandle::okay() const
-{
-    return true;
-}
-
-bool SystemHandle::spin_once()
-{
-    using namespace std::chrono_literals;
-    std::this_thread::sleep_for(100ms);
-    return okay();
-}
-
-bool SystemHandle::subscribe(
-    const std::string& topic_name,
-    const xtypes::DynamicType& message_type,
-    SubscriptionCallback callback,
-    const YAML::Node& /* configuration */)
-{
-    try
-    {
-        auto subscriber = std::make_shared<Subscriber>(
-            participant_.get(), topic_name, message_type, callback);
-
-        subscribers_.emplace_back(std::move(subscriber));
-
-        std::cout << "[soss-dds]: subscriber created. "
-            "topic: " << topic_name << ", "
-            "type: " << message_type.name() << std::endl;
-
+        std::cout << "[soss-dds]: configured!" << std::endl;
         return true;
     }
-    catch(DDSMiddlewareException& e)
-    {
-        std::cerr << "[soss-dds]: " << e.what() << std::endl;
-        return false;
-    }
-}
 
-std::shared_ptr<TopicPublisher> SystemHandle::advertise(
-    const std::string& topic_name,
-    const xtypes::DynamicType& message_type,
-    const YAML::Node& /* configuration */)
-{
-    try
+    bool okay() const
     {
-        auto publisher = std::make_shared<Publisher>(participant_.get(), topic_name, message_type);
-        publishers_.emplace_back(std::move(publisher));
-
-        std::cout << "[soss-dds]: publisher created. "
-            "topic: " << topic_name << ", "
-            "type: " << message_type.name() << std::endl;
-
-        return publishers_.back();
+        return true;
     }
-    catch(DDSMiddlewareException& e)
+
+    bool spin_once()
     {
-        std::cerr << "[soss-dds]: " << e.what() << std::endl;
-        return std::shared_ptr<TopicPublisher>();
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(100ms);
+        return okay();
     }
-}
+
+    bool subscribe(
+        const std::string& topic_name,
+        const xtypes::DynamicType& message_type,
+        SubscriptionCallback callback,
+        const YAML::Node& /* configuration */)
+    {
+        try
+        {
+            auto subscriber = std::make_shared<Subscriber>(
+                participant_.get(), topic_name, message_type, callback);
+
+            subscribers_.emplace_back(std::move(subscriber));
+
+            std::cout << "[soss-dds]: subscriber created. "
+                "topic: " << topic_name << ", "
+                "type: " << message_type.name() << std::endl;
+
+            return true;
+        }
+        catch(DDSMiddlewareException& e)
+        {
+            std::cerr << "[soss-dds]: " << e.what() << std::endl;
+            return false;
+        }
+    }
+
+    std::shared_ptr<TopicPublisher> advertise(
+        const std::string& topic_name,
+        const xtypes::DynamicType& message_type,
+        const YAML::Node& /* configuration */)
+    {
+        try
+        {
+            auto publisher = std::make_shared<Publisher>(participant_.get(), topic_name, message_type);
+            publishers_.emplace_back(std::move(publisher));
+
+            std::cout << "[soss-dds]: publisher created. "
+                "topic: " << topic_name << ", "
+                "type: " << message_type.name() << std::endl;
+
+            return publishers_.back();
+        }
+        catch(DDSMiddlewareException& e)
+        {
+            std::cerr << "[soss-dds]: " << e.what() << std::endl;
+            return std::shared_ptr<TopicPublisher>();
+        }
+    }
+
+private:
+    std::unique_ptr<Participant> participant_;
+    std::vector<std::shared_ptr<Publisher>> publishers_;
+    std::vector<std::shared_ptr<Subscriber>> subscribers_;
+};
+
+
 
 
 } // namespace dds
